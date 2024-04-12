c92f16299736dc2b95a23541c1cd0090c76cc77d
ZonianMidian <57513632+ZonianMidian@users.noreply.github.com>
2024-04-12 00:51:36 -0500

    Moved port declaration to data file

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const data = require('./data.json');
 const express = require('express');
 const utils = require('./utils');
 const app = express();
-const port = 2028;
+
+const port = data.port || 3000;
 
 app.use('/favicon.ico', express.static(`${__dirname}/static/favicon.ico`));
 app.use('/static', express.static(`${__dirname}/static`));
