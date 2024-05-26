76aa4e9360a1b363ad3d0e515307363275834e46
drapsnatt <8496859+drapsnatt@users.noreply.github.com>
2024-05-26 13:12:59 -0500

    Minor changes to improve the code (#9)
    * minor changes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const data = require('./data.json');
-const express = require('express');
-const utils = require('./utils');
-const app = express();
+import express from 'express';
+import data from './data.json' with { type: 'json' };
+import { LogUtils } from './utils.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
 const port = data.port || 3000;
+const utils = new LogUtils();
 
 app.use('/favicon.ico', express.static(`${__dirname}/static/favicon.ico`));
 app.use('/static', express.static(`${__dirname}/static`));
@@ -21,26 +26,33 @@ app.get('/api', async (req, res) => {
 
 app.get('/faq', (req, res) => {
     const instances = data.justlogsInstances;
+
     res.render('faq', { instances: instances });
 });
 
 app.get('/contact', async (req, res) => {
     const userInfo = await utils.getInfo('zonianmidian');
+
     res.render('contact', userInfo);
 });
 
 app.get('/rdr/:channel', async (req, res) => {
     const channel = utils.formatUsername(req.params.channel);
 
-    if (!utils.userChanRegex.test(channel))
+    if (!utils.userChanRegex.test(channel)) {
         return res.render('error', { error: `Invalid channel or channel ID: ${channel}`, code: '' });
+    }
 
     const { pretty } = req.query;
 
     try {
         const instance = await utils.getInstance(channel, null, 'true', pretty);
-        if (instance.error) return res.render('error', { error: instance.error, code: '' });
-        else return res.redirect(instance?.channelLogs?.fullLink[0]);
+
+        if (instance.error) {
+            return res.render('error', { error: instance.error, code: '' });
+        }
+
+        return res.redirect(instance?.channelLogs?.fullLink[0]);
     } catch (err) {
         return res.render('error', { error: `Internal error${err.message ? ` - ${err.message}` : ''}`, code: '' });
     }
@@ -49,18 +61,24 @@ app.get('/rdr/:channel', async (req, res) => {
 app.get('/rdr/:channel/:user', async (req, res) => {
     const channel = utils.formatUsername(req.params.channel);
     const user = utils.formatUsername(req.params.user);
-
-    if (!utils.userChanRegex.test(channel))
-        return res.render('error', { error: `Invalid channel or channel ID: ${channel}`, code: '' });
-    if (!utils.userChanRegex.test(user))
-        return res.render('error', { error: `Invalid username or user ID: ${user}`, code: '' });
-
     const { pretty } = req.query;
+
+    if (!utils.userChanRegex.test(channel)) {
+        return res.render('error', { error: `Invalid channel or channel ID: ${channel}`, code: '' });
+    }
+
+    if (!utils.userChanRegex.test(user)) {
+        return res.render('error', { error: `Invalid username or user ID: ${user}`, code: '' });
+    }
 
     try {
         const instance = await utils.getInstance(channel, user, 'true', pretty);
-        if (instance.error) return res.render('error', { error: instance.error, code: '' });
-        else return res.redirect(instance?.userLogs?.fullLink[0]);
+
+        if (instance.error) {
+            return res.render('error', { error: instance.error, code: '' });
+        }
+
+        return res.redirect(instance?.userLogs?.fullLink[0]);
     } catch (err) {
         return res.render('error', { error: `Internal error${err.message ? ` - ${err.message}` : ''}`, code: '' });
     }
@@ -74,13 +92,21 @@ app.get('/api/:channel', async (req, res) => {
     if (!utils.userChanRegex.test(channel)) error = `Invalid channel or channel ID: ${channel}`;
 
     const isPlain = plain?.toLowerCase() === 'true';
+
     try {
         const instances = await utils.getInstance(channel, null, force, pretty, error);
-        if (isPlain) return res.send(instances?.channelLogs?.fullLink[0] ?? instances?.error);
-        else return res.send(instances);
+
+        if (isPlain) {
+            return res.send(instances?.channelLogs?.fullLink[0] ?? instances?.error);
+        }
+
+        return res.send(instances);
     } catch (err) {
-        if (isPlain) return res.send(`Internal error${err.message ? ` - ${err.message}` : ''}`);
-        else return res.send({ error: `Internal error${err.message ? ` - ${err.message}` : ''}` });
+        if (isPlain) {
+            return res.send(`Internal error${err.message ? ` - ${err.message}` : ''}`);
+        }
+
+        return res.send({ error: `Internal error${err.message ? ` - ${err.message}` : ''}` });
     }
 });
 
@@ -88,19 +114,26 @@ app.get('/api/:channel/:user', async (req, res) => {
     const { force, pretty, plain } = req.query;
     const channel = utils.formatUsername(req.params.channel);
     const user = utils.formatUsername(req.params.user);
+    const isPlain = plain?.toLowerCase() === 'true';
     let error = null;
 
     if (!utils.userChanRegex.test(channel)) error = `Invalid channel or channel ID: ${channel}`;
     if (!utils.userChanRegex.test(user)) error = `Invalid username or user ID: ${user}`;
 
-    const isPlain = plain?.toLowerCase() === 'true';
     try {
         const instances = await utils.getInstance(channel, user, force, pretty, error);
-        if (isPlain) return res.send(instances?.userLogs?.fullLink[0] ?? instances?.error);
-        else return res.send(instances);
+
+        if (isPlain) {
+            res.send(instances?.userLogs?.fullLink[0] ?? instances?.error);
+        }
+
+        return res.send(instances);
     } catch (err) {
-        if (isPlain) return res.send(`Internal error${err.message ? ` - ${err.message}` : ''}`);
-        else return res.send({ error: `Internal error${err.message ? ` - ${err.message}` : ''}` });
+        if (isPlain) {
+            return res.send(`Internal error${err.message ? ` - ${err.message}` : ''}`);
+        }
+
+        return res.send({ error: `Internal error${err.message ? ` - ${err.message}` : ''}` });
     }
 });
 
@@ -109,6 +142,7 @@ app.get('/rm/:channel', async (req, res) => {
 
     try {
         const recentMessages = await utils.getRecentMessages(channel, req.query);
+
         return res.send(recentMessages);
     } catch (err) {
         return res.send({ error: `Internal error${err.message ? ` - ${err.message}` : ''}` });
@@ -122,11 +156,13 @@ app.get('/recent-messages/:channel', function (req, res) {
 app.use(function (req, res, next) {
     const err = new Error('Not found');
     err.status = 404;
+
     next(err);
 });
 
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
     res.status(err.status || 500);
+
     return res.render('error', { error: err.message, code: `${err.status} - ` });
 });
 
